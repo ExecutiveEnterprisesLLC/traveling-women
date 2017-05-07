1c0e4d0f820fc98bf4a8612354b4018bbf9c8fff
Brian Bianchi <bianchibrianj@gmail.com>
2017-05-06 23:05:45 -0400

    db connection

--- review/settings.py
+++ review/settings.py
@@ -19,7 +19,8 @@ DEBUG = True
 #    from secret_settings import *
 #else:
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
