dd573cc0913b1628b45b4d10ce5750c78228bf77
Brian Bianchi <bianchibrianj@gmail.com>
2017-05-24 20:27:36 -0400

    collectstatic

--- review/settings.py
+++ review/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,15 +144,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
     #'/var/www/static/',
-#)
+)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
 
 # crispy form tags
